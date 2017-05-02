ded10f5b6fc391a2c58cf3b72908b0d7a16cac48
Tats <info@sofianaudry.com>
2017-05-02 16:44:30 -0400

    Integrated Neopixel code into bioreactor code.

--- arduino/XenolaliaBioreactor/XenolaliaBioreactor.ino
+++ arduino/XenolaliaBioreactor/XenolaliaBioreactor.ino
@@ -4,6 +4,10 @@
 #include <DS3231_Simple.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
 // Real-time clock.
 DS3231_Simple rtc;
@@ -35,20 +39,30 @@ DS3231_Simple rtc;
 #define HEATER_VALUE_OFF   0
 
 // Transistor control pins.
-#define LIGHT_AOUT    3
-#define STIRRER_AOUT 10
-#define PUMP_AOUT     9
-#define HEATER_AOUT  11
+#define LIGHT_OUT          6 // neopixel
+#define STIRRER_AOUT      10
+#define PUMP_AOUT          9
+#define HEATER_AOUT       11
+#define INDICATOR_LED_OUT 13 // indicator LED is used to show that lights are switched on
+
+// Light-related.
+#define LIGHT_N_PIXELS    24 // n. pixels in the neopixel
+const uint32_t LIGHT_COLOR = Adafruit_NeoPixel::Color(255, 255, 255);
 
 // Temperature control.
 #define TEMPERATURE_ONE_WIRE_BUS 5
+
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LIGHT_N_PIXELS, LIGHT_OUT, NEO_GRB + NEO_KHZ800);
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire temperatureOneWire(TEMPERATURE_ONE_WIRE_BUS);
 
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature temperatureSensor(&temperatureOneWire);
-
 // Timer declarations.
 Chrono pumpTimer;
 Chrono stirrerTimer;
@@ -61,10 +75,11 @@ void setup() {
     }
 
   // Set pins.
-  pinMode(LIGHT_AOUT,   OUTPUT); digitalWrite(LIGHT_AOUT,   LOW);
   pinMode(STIRRER_AOUT, OUTPUT); digitalWrite(STIRRER_AOUT, LOW);
   pinMode(PUMP_AOUT,    OUTPUT); digitalWrite(PUMP_AOUT,    LOW);
   pinMode(HEATER_AOUT,  OUTPUT); digitalWrite(HEATER_AOUT,  LOW);
+  // Init neopixel.
+  pixels.begin();
 
   // Start real-time clock.
   rtc.begin();
@@ -146,7 +161,17 @@ void updateHeater(float temp) {
 void setLight(bool isOn) {
   rtc.printTimeTo_HMS(Serial);
   Serial.print(" :: Set light to "); Serial.println(isOn ? "ON" : "OFF");
-  analogWrite(LIGHT_AOUT, isOn ? LIGHT_VALUE_ON : LIGHT_VALUE_OFF);
+
+  for(int i=0;i<LIGHT_N_PIXELS;i++) {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, LIGHT_COLOR);
+    pixels.setBrightness(isOn ? LIGHT_VALUE_ON : LIGHT_VALUE_OFF);
+   // 610 nm wavelength = 255,155,0 RGB
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+  
+  digitalWrite(INDICATOR_LED_OUT, isOn ? HIGH : LOW);
 }
 
 void setStirrer(bool isOn) {
