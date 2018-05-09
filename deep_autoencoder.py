e2fd37862fb98fc29a31a80168990c1fa8a2c020
Sofian Audry <info@sofianaudry.com>
2018-05-09 17:10:50 -0400

    Complete revision of the deep autoencoder training with args and flexible architecture.

--- deep_autoencoder.py
+++ deep_autoencoder.py
@@ -1,5 +1,18 @@
 # Source: https://blog.keras.io/building-autoencoders-in-keras.html
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("architecture", type=str, help="A comma-separated list of numbers representing the number of neurons in each hidden layer")
+parser.add_argument("model_file", type=str, help="The file where to save the model (in HDF5 format)")
+
+parser.add_argument("-e", "--n-epochs", type=int, default=100, help="Number of epochs")
+parser.add_argument("-b", "--batch-size", type=int, default=256, help="The batch size")
+parser.add_argument("-s", "--show-figure", type=bool, default=True, help="Show example reconstruction after training")
+parser.add_argument("-D", "--model-directory", type=str, default=".", help="The directory where models will be saved")
+
+args = parser.parse_args()
+
 from keras.layers import Input, Dense
 from keras.models import Model, load_model
 
@@ -10,23 +23,26 @@ image_dim = image_side*image_side
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
 # this is our input placeholder
-input_img = Input(shape=(image_dim,))
+input = Input(shape=(image_dim,))
 
-# "encoded" is the encoded representation of the input
-encoded = Dense(encoding_dim*4, activation='relu')(input_img)
-encoded = Dense(encoding_dim*2, activation='relu')(encoded)
-encoded = Dense(encoding_dim,   activation='relu')(encoded)
+layers = args.architecture.split(',')
 
-# "decoded" is the lossy reconstruction of the input
-decoded = Dense(encoding_dim*2, activation='relu')(encoded)
-decoded = Dense(encoding_dim*4, activation='relu')(decoded)
-decoded = Dense(image_dim,      activation='sigmoid')(decoded)
+# build all the layers
+output = input
+for n_hidden in layers:
+    output = Dense(int(n_hidden), activation='relu')(output)
+output = Dense(image_dim, activation='sigmoid')(output)
 
-# this model maps an input to its reconstruction
-autoencoder = Model(input=input_img, output=decoded)
+# maps an input to its reconstruction
+autoencoder = Model(inputs=input, outputs=output)
 
+print(autoencoder.summary())
+
+# compile autoencoder
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
+
+# create training and test sets
 from keras.datasets import mnist
 import numpy as np
 (x_train, _), (x_test, _) = mnist.load_data()
@@ -35,24 +51,22 @@ x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print(x_train.shape)
-print(x_test.shape)
 
 import os.path
 from keras.callbacks import ModelCheckpoint
-filename = "autoencoder.h5"
-if os.path.isfile(filename):
-  print("Loading autoencoder from file")
-  autoencoder = load_model(filename)
-else:
-  print("Training autoencoder")
-  autoencoder.fit(x_train, x_train,
-                  nb_epoch=100,
-                  batch_size=256,
-                  shuffle=True,
-                  callbacks=[ModelCheckpoint("autoencoder.{epoch:02d}.hdf5")],
-                  validation_data=(x_test, x_test))
-  autoencoder.save(filename)
+
+# create models directory if needed
+#if not os.path.exists(args.model_directory):
+#	os.makedirs(args.model_directory)
+
+print("Training autoencoder")
+autoencoder.fit(x_train, x_train,
+                epochs=args.n_epochs,
+                batch_size=args.batch_size,
+                shuffle=True,
+#                callbacks=[ModelCheckpoint(args.model_directory + "/autoencoder.{epoch:02d}.hdf5")],
+                validation_data=(x_test, x_test))
+autoencoder.save(args.model_file)
 
 # encode and decode some digits
 # note that we take them from the *test* set
@@ -61,20 +75,21 @@ decoded_imgs = autoencoder.predict(x_test)
 # use Matplotlib (don't ask)
 import matplotlib.pyplot as plt
 
-n = 10  # how many digits we will display
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # display original
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+if (args.show_figure):
+    n = 10  # how many digits we will display
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # display original
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(x_test[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
-    # display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
+        # display reconstruction
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_imgs[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
