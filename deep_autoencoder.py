240e8d97a1ff52fa43ddef2b4efd664dd038eaf5
Sofian Audry <info@sofianaudry.com>
2018-05-09 15:57:16 -0400

    Changed print calls to make compatible with Python 3.

--- deep_autoencoder.py
+++ deep_autoencoder.py
@@ -35,17 +35,17 @@ x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print x_train.shape
-print x_test.shape
+print(x_train.shape)
+print(x_test.shape)
 
 import os.path
 from keras.callbacks import ModelCheckpoint
 filename = "autoencoder.h5"
 if os.path.isfile(filename):
-  print "Loading autoencoder from file"
+  print("Loading autoencoder from file")
   autoencoder = load_model(filename)
 else:
-  print "Training autoencoder"
+  print("Training autoencoder")
   autoencoder.fit(x_train, x_train,
                   nb_epoch=100,
                   batch_size=256,
