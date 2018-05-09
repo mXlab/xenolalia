44925c9a0291819d6956bd09c0e57b55611e3a75
Sofian Audry <info@sofianaudry.com>
2018-05-09 17:32:16 -0400

    Integrated video generation with changes in deep learning training.

--- autoencoder_generate_video.py
+++ autoencoder_generate_video.py
@@ -1,3 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("model_file", type=str, help="The file containing the trained model")
+
+parser.add_argument("-f", "--fps", type=float, default=30, help="Frames per second")
+parser.add_argument("-d", "--duration", type=float, default=10, help="Duration (in seconds)")
+parser.add_argument("-n", "--n-clips", type=int, default=10, help="Number of video clips to generate")
+parser.add_argument("-D", "--output-directory", type=str, default=".", help="The directory where video clips will be saved")
+
+args = parser.parse_args()
+
+import os
 import numpy as np
 import matplotlib.animation as animation
 
@@ -8,19 +21,18 @@ from pylab import *
 image_side = 28
 image_dim = image_side*image_side
 
-folder = "saved_images"
+fps = args.fps
+n_seconds = args.duration
+n_frames = fps * n_seconds
+interval = 1000.0 / fps
 
-fps = 30
-n_seconds = 10
-n_steps = fps * n_seconds
-
-frame = np.random.random((1,image_dim))
+frame = np.random.random((1, image_dim))
 
 def autoencoder_generate(video_filename):
     global frame, model
     # Generate first image as random
-    frame = np.random.random((1,image_dim))
-    
+    frame = np.random.random((1, image_dim))
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_aspect('equal')
@@ -41,16 +53,18 @@ def autoencoder_generate(video_filename):
         return im
 
     #legend(loc=0)
-    ani = animation.FuncAnimation(fig,update_img,300,interval=30)
-    writer = animation.writers['ffmpeg'](fps=30)
+    ani = animation.FuncAnimation(fig, update_img, n_frames, interval=interval)
+    writer = animation.writers['ffmpeg'](fps=fps)
 
-    ani.save(video_filename,writer=writer,dpi=100)#,savefig_kwargs={ 'bbox_inches': 'tight', 'pad_inches': 0 })
-    return ani
+    ani.save(video_filename, writer=writer, dpi=100)#,savefig_kwargs={ 'bbox_inches': 'tight', 'pad_inches': 0 })
 
-filename = "saved_models_deep/autoencoder.h5"
-#filename = "autoencoder.h5"
-model = load_model(filename)
+# load the model
+model = load_model(args.model_file)
 
-for i in range(0, 20):
-  print "Generating movie # {0:02d}".format(i)
-  autoencoder_generate("output_{0:02d}.mp4".format(i))
+# create models directory if needed
+if not os.path.exists(args.output_directory):
+	os.makedirs(args.output_directory)
+
+for i in range(0, args.n_clips):
+  print("Generating movie # {0:02d}".format(i))
+  autoencoder_generate(args.output_directory + "/output_{0:02d}.mp4".format(i))
