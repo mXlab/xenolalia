35f80e886a5fb7ac92765eff7e008c0fa297fdf2
Sofian Audry <info@sofianaudry.com>
2018-10-13 12:59:46 -0400

    Added licensing info.

--- arduino/XenolaliaBioreactor/XenolaliaBioreactor.ino
+++ arduino/XenolaliaBioreactor/XenolaliaBioreactor.ino
@@ -1,5 +1,21 @@
-/* XENOLALIA project - TeZ + Sofian 2017 */
-
+/*
+ * Xenolalia Bioreactor code
+ *
+ * (c) TeZ + Sofian Audry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <Chrono.h>
 #include <DS3231_Simple.h>
 #include <OneWire.h>
--- arduino/XenolaliaControl/XenolaliaControl.ino
+++ arduino/XenolaliaControl/XenolaliaControl.ino
@@ -1,6 +1,21 @@
-// XENOLALIA NEOPIXELS & SERVO MOTOR OSC CONTROL // TeZ 2018
-////////////////////
-#include <Servo.h> 
+/*
+ * Xenolalia Light + motors control
+ *
+ * (c) TeZ + Sofian Audry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 int servoPin = 0; // WEMOS pin D3
 Servo Servo1; 
 int Sangle = 90; // initial servo angle
