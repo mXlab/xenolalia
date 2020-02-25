921e5b3963f0c0203a5756f1ce8de0d474af35ca
Sofian Audry <info@sofianaudry.com>
2020-02-25 08:15:48 -0500

    Install script (for the moment just sets cron job for syncing files)

---
+++ xeno_pi_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# -ne 2 ];
+then
+ echo "Syntax: $(basename $0) <ftp_username> <ftp_password>"
+ exit 1
+fi
+
+ftp_username="$1"
+ftp_password="$2"
+xeno_dir=`pwd`
+cmd_sync_snapshots="$xeno_dir/sync_snapshots.sh"
+cron_sync_snapshots="/etc/cron.hourly/xeno_sync_snapshots"
+
+# Create cron job.
+echo "/bin/bash $cmd_sync_snapshots $ftp_username $ftp_password" > $cron_sync_snapshots
+chmod u+x $cron_sync_snapshots
