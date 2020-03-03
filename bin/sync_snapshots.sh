ebe58919689fbad30d58162df2827b63cb7ebe4c
Sofian Audry <info@sofianaudry.com>
2020-03-03 05:28:32 -0500

    Fix snapshot syncing to cd to the snapshot folder upstream

--- bin/sync_snapshots.sh
+++ bin/sync_snapshots.sh
@@ -7,5 +7,6 @@ fi
 
 lftp -c "set ftp:list-options -a;
 open ftp://$1:$2@ftp.koumbit.net; 
+cd snapshots;
 lcd /home/pi/xenolalia/XenoPi/snapshots;
 mirror --reverse --use-cache --allow-chown --allow-suid --no-umask --parallel=2 --exclude-glob .git"
